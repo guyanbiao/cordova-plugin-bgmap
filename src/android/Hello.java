59d7f16bb0488959ad7e724bcfd98ac6f542764a
Groot <groot@sinfotech.cn>
2015-08-09 13:47:18 +0800

    test

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -3,6 +3,11 @@ package com.example.plugin;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import android.content.Intent;
+import java.io.File;
+import android.util.Log;
+import java.net.URISyntaxException;
+
 
 public class Hello extends CordovaPlugin {
 
@@ -11,10 +16,17 @@ public class Hello extends CordovaPlugin {
 
         if (action.equals("greet")) {
 
-            String name = data.getString(0);
-            String message = "Hello, " + name;
-            callbackContext.success(message);
-
+          try {
+            Intent intent = Intent.getIntent("intent://map/direction?origin=latlng:34.264642646862,108.95108518068|name:我家&destination=大雁塔&mode=driving®ion=西安&referer=Autohome|GasStation#Intent;scheme=bdapp;package=com.baidu.BaiduMap;end");
+            if(isInstallByread("com.baidu.BaiduMap")){
+              cordova.getActivity().startActivity(intent); //启动调用
+              Log.e("GasStation", "百度地图客户端已经安装") ;
+            }else{
+              Log.e("GasStation", "没有安装百度地图客户端") ;
+            }
+          } catch (URISyntaxException e) {
+            e.printStackTrace();
+          }
             return true;
 
         } else {
@@ -23,4 +35,9 @@ public class Hello extends CordovaPlugin {
 
         }
     }
+
+    private boolean isInstallByread(String packageName) {   
+      return new File("/data/data/" + packageName).exists();   
+    }
+
 }
